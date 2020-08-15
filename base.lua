472c0e0eb1ff9e97ef99875f37b17ae8811ec560
Sertyy <69120818+Sertyy@users.noreply.github.com>
2020-08-15 17:43:05 +0200

    Create base.lua

---
+++ base.lua
@@ -0,0 +1,15 @@
+Voici la base de Full Life que je leak BASE FULL OPTI POUR 200 joueurs !!
+
+Faut désactiver le script ILLIES ! pour que le serveur MARCHE. 
+
+JE EZ ILLIES JAMAI <3 dédicase à toi ! 🍑🍑🍑🍑 qui fait le développeur en disant que c'est sa base et que c'est lui qui la dev ^^
+
+https://gofile.io/d/mi9iA5 = BASE A TELECHARGER AVEC LE CFG / et le resource ! 
+
+
+https://cdn.discordapp.com/attachments/719962950496157747/742037747723927572/fullifepd.sql  = SQL DE FULL LIFE ! 
+
+
+Base by Sertyy#7777 ou Zynaro#0001
+
+Si vous avez besoin d'aide MP MOI ! 
