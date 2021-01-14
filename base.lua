df245878ffac3cfb8a431e5ba42ad025c77ac36d
Sertyy <69120818+Sertyy@users.noreply.github.com>
2021-01-14 20:03:46 +0100

    Update base.lua

--- base.lua
+++ base.lua
@@ -2,7 +2,7 @@ Voici la base de Full Life que je leak BASE FULL OPTI POUR 200 joueurs !!
 
 Faut désactiver le script ILLIES ! pour que le serveur MARCHE. 
 
-JE EZ ILLIES JAMAI <3 dédicase à toi ! 🍑🍑🍑🍑 qui fait le développeur en disant que c'est sa base et que c'est lui qui la dev ^^
+ILLIES JAMAI <3 dédicase à toi ! 🍑🍑🍑🍑 qui fait le développeur en disant que c'est sa base et que c'est lui qui la dev ^^
 
 https://gofile.io/d/mi9iA5 = BASE A TELECHARGER AVEC LE CFG / et le resource ! 
 
@@ -10,6 +10,6 @@ https://gofile.io/d/mi9iA5 = BASE A TELECHARGER AVEC LE CFG / et le resource !
 https://cdn.discordapp.com/attachments/746383744566427798/746385016530927717/cyrils59895fivem.sql = SQL DE FULL LIFE ! 
 
 
-Base by ! Sertyy#4444
+Base by ! $ERTYY#0001
 
-Si vous avez besoin d'aide MP MOI ou Zynaro#0001
+Si vous avez besoin d'aide MP MOI.  $ERTYY#0001
